c6f076e4dbc7f7f26028e0ef7121658b3363b4a2
wyattsweet <wyattsweet@gmail.com>
2018-02-10 10:32:06 -0800

    updated code to use async await

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import bcrypt from 'bcrypt'
 import bodyParser from 'body-parser'
 import express from 'express'
@@ -11,23 +12,27 @@ const app = express()
 const usersApi = new UsersApi()
 
 app.use(bodyParser.json())
-// app.use(express.static(path.join(__dirname, '/public')))
-// app.use()
+app.use(express.static(path.join(__dirname, '/public')))
 
-// app.get('/', (req, res) => {
-// })
+// passes any errors that happen in await function to
+// expresses default error handling
+const asyncMiddleware = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next)
+}
 
-app.post('/api/v1/user', (req, res) => {
-  const reqData = req.body
-  bcrypt.hash(reqData.password, 10).then(hash => {
-    usersApi.createUser(reqData.email, String(hash)).then(data => {
-      const cert = fs.readFileSync('jwtRS256.key', 'utf8')
-      const token = jwt.sign({sub: 1, email: 'joe@shmo.com'}, cert, {
-        algorithm: 'RS256'
-      })
-      res.send(token)
+app.post(
+  '/api/v1/user',
+  asyncMiddleware(async (req, res) => {
+    const reqData = req.body
+    const hash = await bcrypt.hash(reqData.password, 10)
+    const userData = await usersApi.createUser(reqData.email, String(hash))
+    console.log(userData)
+    const cert = fs.readFileSync('jwtRS256.key', 'utf8')
+    const token = jwt.sign({sub: 1, email: 'joe@shmo.com'}, cert, {
+      algorithm: 'RS256'
     })
+    res.send(token)
   })
-})
+)
 
 app.listen(3000)
